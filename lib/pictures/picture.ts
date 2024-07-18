4fcd3608fe50f5978a66b44c0a9cf961ac09e27b
Jacob Heric <jacob@jacobheric.com>
2024-07-18 10:15:42 -0400

    reorg pics so it's just local

--- lib/pictures/picture.ts
+++ lib/pictures/picture.ts
@@ -1,14 +1,10 @@
-import { existsSync } from "@std/fs";
-
-import sharp from "npm:/sharp";
 import { join } from "jsr:@std/path@0.213/join";
 
-const RAW_POST_PICS_DIR = "./static/image/posts/raw";
-const OUT_POST_PICS_DIR = "./static/image/posts/";
+export const RAW_POST_PICS_DIR = "./static/image/posts/raw";
+export const OUT_POST_PICS_DIR = "./static/image/posts/";
 
 export const IMG_SMALL = 640;
 export const IMG_LARGE = 1300;
-const QUALITY = 85;
 
 export const postPics = () => Array.from(Deno.readDirSync(RAW_POST_PICS_DIR));
 
@@ -23,57 +19,3 @@ export const largeName = (name: string, dir?: string) =>
 
 export const smallName = (name: string, dir?: string) =>
   join(dir || OUT_POST_PICS_DIR, name.replace(".jpg", `-small.jpg`));
-
-const resize = async (
-  img: sharp.Sharp,
-  name: string,
-) => {
-  console.log("resizing image", name);
-  img
-    .withMetadata()
-    .toFormat("jpeg")
-    .jpeg({ quality: QUALITY })
-    .toFile(originalName(name));
-
-  const w = (await img.metadata()).width;
-
-  w && w > IMG_LARGE && img.resize({ width: IMG_LARGE })
-    .withMetadata()
-    .toFormat("jpeg")
-    .jpeg({ quality: QUALITY })
-    .toFile(largeName(name));
-
-  w && w > IMG_SMALL && img.resize({ width: IMG_SMALL })
-    .withMetadata()
-    .toFormat("jpeg")
-    .jpeg({ quality: QUALITY })
-    .toFile(smallName(name));
-};
-
-export const resizeAll = async () => {
-  const pics = postPics();
-
-  await Promise.all(pics.map(async (p: Deno.DirEntry) => {
-    if (p.name.endsWith(".gif")) {
-      await Deno.copyFile(
-        join(RAW_POST_PICS_DIR, p.name),
-        join(OUT_POST_PICS_DIR, p.name),
-      );
-      return;
-    }
-
-    if (
-      existsSync(originalName(p.name)) && existsSync(largeName(p.name)) &&
-      existsSync(smallName(p.name))
-    ) {
-      return;
-    }
-
-    const raw: Uint8Array = await Deno.readFile(
-      join(RAW_POST_PICS_DIR, p.name),
-    );
-
-    const img = sharp(raw);
-    void resize(img, p.name);
-  }));
-};
--- lib/pictures/resize.ts
+++ lib/pictures/resize.ts
@@ -1,4 +1,73 @@
-import { resizeAll } from "@/lib/pictures/picture.ts";
+import { existsSync } from "@std/fs";
+
+import sharp from "npm:/sharp";
+import { join } from "https://deno.land/std@0.216.0/path/join.ts";
+import {
+  IMG_LARGE,
+  IMG_SMALL,
+  largeName,
+  originalName,
+  OUT_POST_PICS_DIR,
+  postPics,
+  RAW_POST_PICS_DIR,
+  smallName,
+} from "@/lib/pictures/picture.ts";
+
+const QUALITY = 85;
+
+const resize = async (
+  img: sharp.Sharp,
+  name: string,
+) => {
+  console.log("resizing image", name);
+  img
+    .withMetadata()
+    .toFormat("jpeg")
+    .jpeg({ quality: QUALITY })
+    .toFile(originalName(name));
+
+  const w = (await img.metadata()).width;
+
+  w && w > IMG_LARGE && img.resize({ width: IMG_LARGE })
+    .withMetadata()
+    .toFormat("jpeg")
+    .jpeg({ quality: QUALITY })
+    .toFile(largeName(name));
+
+  w && w > IMG_SMALL && img.resize({ width: IMG_SMALL })
+    .withMetadata()
+    .toFormat("jpeg")
+    .jpeg({ quality: QUALITY })
+    .toFile(smallName(name));
+};
+
+export const resizeAll = async () => {
+  const pics = postPics();
+
+  await Promise.all(pics.map(async (p: Deno.DirEntry) => {
+    if (p.name.endsWith(".gif")) {
+      await Deno.copyFile(
+        join(RAW_POST_PICS_DIR, p.name),
+        join(OUT_POST_PICS_DIR, p.name),
+      );
+      return;
+    }
+
+    if (
+      existsSync(originalName(p.name)) && existsSync(largeName(p.name)) &&
+      existsSync(smallName(p.name))
+    ) {
+      return;
+    }
+
+    const raw: Uint8Array = await Deno.readFile(
+      join(RAW_POST_PICS_DIR, p.name),
+    );
+
+    const img = sharp(raw);
+    void resize(img, p.name);
+  }));
+};
 
 console.log("resizing...");
 await resizeAll();
