773755ac8ad002dfef0ca761407c515c364557e1
Jacob Heric <jacob@jacobheric.com>
2024-07-21 11:39:57 -0400

    fixup books page

--- routes/books.tsx
+++ routes/books.tsx
@@ -1,15 +1,17 @@
+import { Picture } from "@/components/Picture.tsx";
+
 export default function Books() {
   return (
     <div class="w-11/12 mx-auto flex flex-col items-center">
       <div class="title text-6xl my-12 text-center">Books</div>
       <div>
-        <span class="title text-2xl font-bold italic inline mr-2">
+        <span class="title text-2xl font-bold italic inline mr-2 text-center">
           Autobiography of Joker So Far
         </span>
         <span class="inline">by Jacob Heric</span>
       </div>
       <div class="flex flex-row justify-center font-bold text-2xl my-8 space-x-2">
-        <div>
+        <div class="w-full">
           <a
             href="https://www.amazon.com/Autobiography-Joker-Far-Jacob-Heric-ebook/dp/B018B79GEY/"
             rel="nofollow"
@@ -17,10 +19,10 @@ export default function Books() {
             kindle
           </a>
         </div>
-        <div>
+        <div class="w-full">
           &#124;
         </div>
-        <div>
+        <div class="w-full">
           <a
             href="https://www.amazon.com/Autobiography-Joker-Far-Jacob-Heric/dp/0692539506/"
             rel="nofollow"
@@ -30,22 +32,22 @@ export default function Books() {
         </div>
       </div>
 
-      <div class="flex flex-row flex-wrap md:flex-nowrap justify-center space-x-2">
-        <div className="w-1/2">
-          <img
-            className="max-w-xl ml-auto mr-4"
-            src={`/image/posts/raw/auto-bio-cover-large.jpg`}
+      <div class="flex flex-row flex-wrap md:flex-nowrap justify-center align-top space-x-2">
+        <div className="w-full flex justify-center lg:justify-end">
+          <Picture
+            className="max-w-xl  mr-4  mt-0"
+            src="auto-bio-cover.jpg"
             alt="Autobiograph of Joker so Far Cover Image"
           />
         </div>
-        <div class="flex-col w-1/2">
-          <div class="font-bold pb-3">
+        <div class="flex-col w-full flex items-center items-end">
+          <div class="font-bold pb-3 max-w-prose">
             Out of the ordinary folds of middle America, comes a teenage obscura
             with a penchant for pain. Joseph Kare, aka{" "}
             <em>Joker</em>, wakes up one day to find himself squarely in the
             crosshairs of cultural change.
           </div>
-          <div>
+          <div className="max-w-prose">
             For Joker, ditching high school in America’s heartland comes with
             dire consequences. His first dilemma is to run damage control - a
             rumor spreads after last night’s party. His next dilemma is Cass.
@@ -61,7 +63,7 @@ export default function Books() {
           </div>
         </div>
       </div>
-      <div class="my-16 text-2xl italic">
+      <div class="my-10 text-2xl italic">
         Out now on
         <a
           href="https://www.truejokercompany.com"
