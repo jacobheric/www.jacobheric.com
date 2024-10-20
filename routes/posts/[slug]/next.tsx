18b4f96dceabe1f02d0fdeec66ef8acb73dd2ffd
Jacob Heric <jacob@jacobheric.com>
2024-10-20 17:20:21 -0400

    fix next/prev bug introduced in upgrade

--- routes/posts/[slug]/next.tsx
+++ routes/posts/[slug]/next.tsx
@@ -1,10 +1,24 @@
-import { PageProps } from "fresh";
+import { HttpError, page, PageProps } from "fresh";
 
 import Post from "@/components/Post.tsx";
+import { getPost } from "@/lib/posts/posts.ts";
+import { define } from "@/lib/state.ts";
 import type { PostPage } from "@/routes/posts/[slug]/index.tsx";
-import { handler as postHandler } from "./index.tsx";
 
-export const handler = postHandler;
+export const handler = define.handlers({
+  GET(ctx) {
+    try {
+      const data = getPost(ctx.params.slug, 1);
+      ctx.state.title = data.post.title;
+      ctx.state.description = data.post.description;
+
+      return page(data);
+    } catch (e: unknown) {
+      console.error("post not found", e);
+      throw new HttpError(404);
+    }
+  },
+});
 
 export default function NextPage(
   { data: { post, hasNext, hasPrev } }: PageProps<PostPage>,
